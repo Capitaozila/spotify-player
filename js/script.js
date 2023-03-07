31404acfe406dc3d511acbaa4e2e850b38e35e59
capitaozila <luis2001sousa@gmail.com>
2023-03-07 19:45:52 -0300

    js

--- js/script.js
+++ js/script.js
@@ -1,41 +1,22 @@
 const songName = document.getElementById("song-name");
 
 function createSong(name, artist, file, liked) {
-  return {
-    songName: name,
-    artist: artist,
-    file: file,
-    liked: liked,
-  };
+    return {
+        songName: name, artist: artist, file: file, liked: liked,
+    };
 }
 
-const isNotTheSameAnimore = createSong(
-  "Untitled",
-  "Rex Orange County",
-  "musica_1",
-  false
-);
+const isNotTheSameAnimore = createSong("Untitled", "Rex Orange County", "musica_1", false);
 
 const karma = createSong("Karma", "Taylor Swift", "musica_2", false);
 
-const untitled = createSong(
-  "I'ts Not The Same Anymore",
-  "Rex Orange County",
-  "musica_3",
-  false
-);
+const untitled = createSong("I'ts Not The Same Anymore", "Rex Orange County", "musica_3", false);
 
 const literalMe = createSong("Literal Me", "B3AST", "musica_4", false);
 
 const snowfall = createSong("Snowfall", "Dreamscape", "musica_5", false);
 
-const originalPlaylist = [
-  isNotTheSameAnimore,
-  karma,
-  untitled,
-  literalMe,
-  snowfall,
-];
+const originalPlaylist = JSON.parse(localStorage.getItem("playlist")) ?? [isNotTheSameAnimore, karma, untitled, literalMe, snowfall];
 
 let sortedPlaylist = [...originalPlaylist];
 let index = 0;
@@ -59,143 +40,143 @@ let isShuffle = false;
 let repeatOn = false;
 
 function playSong() {
-  play.querySelector(".bi").classList.remove("bi-play-circle");
-  play.querySelector(".bi").classList.add("bi-pause-circle");
-  song.play();
-  isPlaying = true;
+    play.querySelector(".bi").classList.remove("bi-play-circle");
+    play.querySelector(".bi").classList.add("bi-pause-circle");
+    song.play();
+    isPlaying = true;
 }
 
 function pauseSong() {
-  play.querySelector(".bi").classList.add("bi-play-circle");
-  play.querySelector(".bi").classList.remove("bi-pause-circle");
-  song.pause();
-  isPlaying = false;
+    play.querySelector(".bi").classList.add("bi-play-circle");
+    play.querySelector(".bi").classList.remove("bi-pause-circle");
+    song.pause();
+    isPlaying = false;
 }
 
 function playPauseDecider() {
-  if (isPlaying === true) {
-    pauseSong();
-  } else {
-    playSong();
-  }
+    if (isPlaying === true) {
+        pauseSong();
+    } else {
+        playSong();
+    }
 }
 
 function renderLikedButton() {
-  if (sortedPlaylist[index].liked === true) {
-    likedButton.querySelector(".bi").classList.remove("bi-heart");
-    likedButton.querySelector(".bi").classList.add("bi-heart-fill");
-    likedButton.classList.add("active");
-  } else {
-    likedButton.querySelector(".bi").classList.add("bi-heart");
-    likedButton.querySelector(".bi").classList.remove("bi-heart-fill");
-    likedButton.classList.remove("active");
-  }
+    if (sortedPlaylist[index].liked === true) {
+        likedButton.querySelector(".bi").classList.remove("bi-heart");
+        likedButton.querySelector(".bi").classList.add("bi-heart-fill");
+        likedButton.classList.add("active");
+    } else {
+        likedButton.querySelector(".bi").classList.add("bi-heart");
+        likedButton.querySelector(".bi").classList.remove("bi-heart-fill");
+        likedButton.classList.remove("active");
+    }
 }
 
 function likeButtonClicked() {
-  if (sortedPlaylist[index].liked === false) {
-    sortedPlaylist[index].liked = true;
-  } else {
-    sortedPlaylist[index].liked = false;
-  }
-  renderLikedButton();
+    if (sortedPlaylist[index].liked === false) {
+        sortedPlaylist[index].liked = true;
+    } else {
+        sortedPlaylist[index].liked = false;
+    }
+    renderLikedButton();
 
-  localStorage.setItem("playlist", JSON.stringify(originalPlaylist));
+    localStorage.setItem("playlist", JSON.stringify(originalPlaylist));
 }
 
 function loadSong() {
-  cover.src = `../fotos/${sortedPlaylist[index].file}.jpg`;
-  song.src = `../musicas/${sortedPlaylist[index].file}.mp3`;
-  songName.innerText = sortedPlaylist[index].songName;
-  bandName.innerText = sortedPlaylist[index].artist;
-  renderLikedButton();
+    cover.src = `../fotos/${sortedPlaylist[index].file}.jpg`;
+    song.src = `../musicas/${sortedPlaylist[index].file}.mp3`;
+    songName.innerText = sortedPlaylist[index].songName;
+    bandName.innerText = sortedPlaylist[index].artist;
+    renderLikedButton();
 }
 
 function previousSong() {
-  if (index === 0) {
-    index = sortedPlaylist.length - 1;
-  } else {
-    index--;
-  }
-  loadSong();
-  playSong();
+    if (index === 0) {
+        index = sortedPlaylist.length - 1;
+    } else {
+        index--;
+    }
+    loadSong();
+    playSong();
 }
 
 // função nova música
 
 function nextSong() {
-  if (index === sortedPlaylist.length - 1) {
-    index = 0;
-  } else {
-    index++;
-  }
-  loadSong();
-  playSong();
+    if (index === sortedPlaylist.length - 1) {
+        index = 0;
+    } else {
+        index++;
+    }
+    loadSong();
+    playSong();
 }
 
 function updateProgress() {
-  const barWidth = (song.currentTime / song.duration) * 100;
-  currentProgress.style.setProperty("--progress", `${barWidth}%`);
-  songCurrentTime.innerText = toHoursMinutesSeconds(song.currentTime);
+    const barWidth = (song.currentTime / song.duration) * 100;
+    currentProgress.style.setProperty("--progress", `${barWidth}%`);
+    songCurrentTime.innerText = toHoursMinutesSeconds(song.currentTime);
 }
 
 function jumpTo(e) {
-  const width = progressContainer.clientWidth;
-  const clickPosition = e.offsetX;
-  song.currentTime = (clickPosition / width) * song.duration;
+    const width = progressContainer.clientWidth;
+    const clickPosition = e.offsetX;
+    song.currentTime = (clickPosition / width) * song.duration;
 }
 
 function shuffleArray(array) {
-  for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
-  }
-  return array;
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
 }
 
 function shuffleButtonClicked() {
-  if (!isShuffle) {
-    isShuffle = true;
-    shuffleArray(sortedPlaylist);
-    shuffleButton.classList.add("active");
-  } else {
-    isShuffle = false;
-    sortedPlaylist = [...originalPlaylist];
-    shuffleButton.classList.remove("active");
-  }
+    if (!isShuffle) {
+        isShuffle = true;
+        shuffleArray(sortedPlaylist);
+        shuffleButton.classList.add("active");
+    } else {
+        isShuffle = false;
+        sortedPlaylist = [...originalPlaylist];
+        shuffleButton.classList.remove("active");
+    }
 }
 
 function repeatButtonClicked() {
-  if (!repeatOn) {
-    repeatOn = true;
-    repeatButton.classList.add("active");
-  } else {
-    repeatOn = false;
-    repeatButton.classList.remove("active");
-  }
+    if (!repeatOn) {
+        repeatOn = true;
+        repeatButton.classList.add("active");
+    } else {
+        repeatOn = false;
+        repeatButton.classList.remove("active");
+    }
 }
 
 function nextOrRepeat() {
-  if (repeatOn) {
-    playSong();
-  } else {
-    nextSong();
-  }
+    if (repeatOn) {
+        playSong();
+    } else {
+        nextSong();
+    }
 }
 
 function updateTotalTime() {
-  toHoursMinutesSeconds(song.duration);
-  songTotalTime.innerText = toHoursMinutesSeconds(song.duration);
+    toHoursMinutesSeconds(song.duration);
+    songTotalTime.innerText = toHoursMinutesSeconds(song.duration);
 }
 
 function toHoursMinutesSeconds(originalTime) {
-  let hours = Math.floor(originalTime / 3600);
-  let minutes = Math.floor((originalTime - hours * 3600) / 60);
-  let seconds = Math.floor(originalTime - hours * 3600 - minutes * 60);
+    let hours = Math.floor(originalTime / 3600);
+    let minutes = Math.floor((originalTime - hours * 3600) / 60);
+    let seconds = Math.floor(originalTime - hours * 3600 - minutes * 60);
 
-  return `${minutes.toString().padStart(2, "0")}:${seconds
-    .toString()
-    .padStart(2, "0")}`;
+    return `${minutes.toString().padStart(2, "0")}:${seconds
+        .toString()
+        .padStart(2, "0")}`;
 }
 
 loadSong();
